d1a3c02813a6a69d909ab80ba3101689fde77e3c
Matias Kinnunen <matias.kinnunen@gmail.com>
2016-08-31 16:30:18 +0300

    Add scoring logic for both players

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,4 +1,31 @@
-
 public class TennisGame {
-
+	private enum Score {
+		LOVE, FIFTEEN, THIRTY, FORTY, ADVANTAGE, WIN;
+		
+		public Score next() {
+			return Score.values()[this.ordinal() + 1];
+		}
+	}
+	
+	private Score[] scores = { Score.LOVE, Score.LOVE };
+	
+	public static final int PLAYER_1 = 0;
+	public static final int PLAYER_2 = 1;
+	
+	public void playerScored(int player) {
+		// Switch 0 -> 1 or vice versa
+		int otherPlayer = player ^ 1;
+		
+		if (scores[otherPlayer] == Score.ADVANTAGE) {
+			// It's deuce again
+			scores[otherPlayer] = Score.FORTY;
+			return;
+		}
+	
+		if (scores[player] == Score.FORTY && scores[otherPlayer] != Score.FORTY) {
+			scores[player] = scores[player].next().next();
+		} else {
+			scores[player] = scores[player].next();
+		}
+	}
 }
