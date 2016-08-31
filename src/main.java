68444007cbf0b3bc9ce2a797ded82da6176c1cd3
idomingo <idomingo@STUPID-PC412.stnet.local>
2016-08-31 16:32:15 +0300

    first lab

---
+++ src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		TennisGame game = new TennisGame();
+		Player player1 = new Player ();
+		Player player2 = new Player ();
+		game.randomGame(player1, player2);
+		
+	}
+
+}
