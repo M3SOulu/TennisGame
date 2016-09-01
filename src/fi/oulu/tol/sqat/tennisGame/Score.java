2dd2cbe101e4b4c085c923fe12173aabef79af9d
heimonsa <heimonsa@STUPID-PC725.stnet.local>
2016-09-01 15:39:48 +0300

    Fixed type on score mapping.

--- src/fi/oulu/tol/sqat/tennisGame/Score.java
+++ src/fi/oulu/tol/sqat/tennisGame/Score.java
@@ -4,7 +4,7 @@ public class Score {
 	
 	private String[] scoreText = {"love", "fifteen", "thirty", "fourty", "advantage"};
 	public static int LOVE = 0;
-	public static int FIFTEEN = 2;
+	public static int FIFTEEN = 1;
 	public static int THIRTY = 2;
 	public static int FOURTY = 3;
 	public static int ADVANTAGE = 4;
