8fe2299eab39f7af203172bbc7753e3918d12cdf
kuzdoan <kuzdoan@STUPID-PC705.stnet.local>
2016-09-01 16:10:27 +0300

    Main method for testing

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TennisGame game = new TennisGame();
+		game.playerScored(2);
+		game.playerScored(1);
+		game.playerScored(2);
+		game.playerScored(2);
+		game.playerScored(1);
+		game.playerScored(2);
+		game.playerScored(2);
+		game.playerScored(1);
+		game.playerScored(2);
+		game.playerScored(1);
+		game.playerScored(1);
+		game.playerScored(1);
+	}
+
+}
