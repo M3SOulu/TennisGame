b1b664f165fc769caa564c6439d27dcde59d73f5
arruokam <arruokam@STUPID-PC706.stnet.local>
2016-09-01 16:21:27 +0300

    Working and tested awesome Tennisgame class with my rusty Java skills.

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -14,15 +14,14 @@ public class TennisGame {
 		} else {
 			player2Score++;
 		}
-		String gameScore = getGameScore();
+		System.out.println(getGameScore());
 	}
 	
 	public String getGameScore() {
-
-		String gameScore = new String("");
-		int delta = Math.abs(player1Score - player2Score);
+		int difference = Math.abs(player1Score - player2Score);
+		int topScore = Math.max(player1Score, player2Score);
 		
-		if (delta >= 2) {
+		if (topScore >= 4 && difference >= 2) {
 			if (player1Score > player2Score) {
 				return new String("Player 1 won");
 			} else {
@@ -33,18 +32,40 @@ public class TennisGame {
 	}
 	
 	public String getStringForPlayerScore(int player, int score) {
-		String name = player == 1 ? new String("Player 1") : new String("Player 2");
+		String name = (player == 1) ? new String("Player 1") : new String("Player 2");
 		String result = new String("");
-		if (score == 1) {
-			result = new String("Love");
-		} else if (score == 2) {
-			result = new String("15");
-		} else if (score == 3) {
-			result = new String("30");
-		} else if (score == 4) {
-			result = new String("40");
+		if (score > 0) {
+			if (score == 1) {
+				result = new String(" Love");
+			} else if (score == 2) {
+				result = new String(" 30");
+			} else if (score == 3) {
+				result = new String(" 40");
+			} else {
+				if (score >= Math.max(player1Score, player2Score)) {
+					result = new String(" Advantage");
+				} else {
+					result = new String(" 40");
+				}
+			}
 		} else {
-			result = new String("Advantage");
+			result = new String(" 0");
 		}
+		return name + result;
+	}
+	
+	
+	public static void playGame() {
+		// Demonstrate simple game
+		TennisGame game = new TennisGame();
+		game.startGame();
+		game.playerScored(2);
+		game.playerScored(2);
+		game.playerScored(2);
+		game.playerScored(2);
+	}
+	
+	public static void main(String[] args) {
+		playGame();
 	}
 }
