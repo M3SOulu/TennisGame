818c4897a93a43c0f8182c58804a24ea7f5abbaf
anrehman <anrehman@STUPID-PC716.stnet.local>
2016-09-01 16:01:53 +0300

    Full Implementation of the requirements for tennis game program

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -7,42 +7,75 @@ public class TennisGame {
 	private final String SCORE_THREE = "40";
 	private final String SAME_SCORE = "deuce";
 	private final String ADVANTAGE = "Advantage";
+	private final String PLAYER_ONE_WINS = "Player1 Wins";
+	private final String PLAYER_TWO_WINS = "Player2 Wins";
 
-	String player1Score = SCORE_ZERO;
-	String player2Score = SCORE_ZERO;
+	int player1Score = 0;
+	int player2Score = 0;
 
-	public String getScore() {
+	public String getCurrentScore() {
 		String result = null;
-		if (player1Score.equals(player2Score)) {
-
+		if (player1Score == player2Score) {
+			result = SAME_SCORE;
+		} else if (player1Score - player2Score >= 2 && player1Score > 3) {
+			result = PLAYER_ONE_WINS;
+		} else if (player2Score - player1Score >= 2 && player2Score > 3) {
+			result = PLAYER_TWO_WINS;
 		} else {
-			result = player1Score + "-" + player2Score;
+			result = getScoreToDisplay(player1Score) + "-" + getScoreToDisplay(player2Score);
 		}
 		return result;
 	}
 
 	public void player1Scored() {
-		if (player1Score.equals(SCORE_ZERO)) {
-			player1Score = SCORE_ONE;
-		} else if (player1Score.equals(SCORE_ONE)) {
-			player1Score = SCORE_TWO;
-		} else if (player1Score.equals(SCORE_TWO)) {
-			player1Score = SCORE_THREE;
-		} else if (player1Score.equals(SCORE_THREE)) {
-
-		}
+		player1Score++;
 	}
 
 	public void player2Scored() {
-		if (player2Score.equals(SCORE_ZERO)) {
-			player2Score = SCORE_ONE;
-		} else if (player2Score.equals(SCORE_ONE)) {
-			player2Score = SCORE_TWO;
-		} else if (player2Score.equals(SCORE_TWO)) {
-			player2Score = SCORE_THREE;
-		} else if (player2Score.equals(SCORE_THREE)) {
-
-		}
+		player2Score++;
 	}
 
+	public String getScoreToDisplay(int score) {
+		String result = "";
+		switch (score) {
+		case 0:
+			result = SCORE_ZERO;
+			break;
+		case 1:
+			result = SCORE_ONE;
+			break;
+		case 2:
+			result = SCORE_TWO;
+			break;
+		case 3:
+			result = SCORE_THREE;
+			break;
+		case 4:
+			result = ADVANTAGE;
+			break;
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		TennisGame oneTennisGame = new TennisGame();
+		oneTennisGame.player1Scored();
+		oneTennisGame.player1Scored();
+		oneTennisGame.player1Scored();
+		oneTennisGame.player2Scored();
+		oneTennisGame.player1Scored();
+		System.out.println("Current Score is: " + oneTennisGame.getCurrentScore());
+	}
 }
+
+
+
+/*
+ * if (player1Score.equals(SCORE_ZERO)) { player1Score = SCORE_ONE; }
+ * else if (player1Score.equals(SCORE_ONE)) { player1Score = SCORE_TWO;
+ * } else if (player1Score.equals(SCORE_TWO)) { player1Score =
+ * SCORE_THREE; } else if (player1Score.equals(SCORE_THREE)) {
+ * 
+ * }
+ */
