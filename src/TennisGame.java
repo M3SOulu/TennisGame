2fc7db0b526ec81b8f30e8be00b744bbb6f2b588
arruokam <arruokam@STUPID-PC706.stnet.local>
2016-09-01 15:38:55 +0300

    First take with Java for a long time: a barebone Tennisgame class.

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,4 +1,35 @@
 
 public class TennisGame {
 
+	private int player1Score;
+	private int player2Score;
+	
+	public void startGame() {
+		player1Score = player2Score = 0;
+	}
+	
+	public void playerScored(int player) {
+		if (player == 1) {
+			player1Score++;
+		} else {
+			player2Score++;
+		}
+		String gameScore = getGameScore();
+	}
+	
+	public String getGameScore() {
+
+		String gameScore = new String("");
+		int delta = Math.abs(player1Score - player2Score);
+		
+		if (delta >= 2) {
+			if (player1Score > player2Score) {
+				gameScore = new String("Player 1 won");
+			} else {
+				gameScore = new String("Player 2 won");
+			}
+		}
+		return new String("foo not complete");
+		
+	}
 }
