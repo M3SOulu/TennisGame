de343d16d155360ad18e3d8d97575c542576c86d
jannelau <jannelau@STUPID-PC709.stnet.local>
2016-09-01 16:14:20 +0300

    testing still

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,13 +1,15 @@
 
+
+
 public class TennisGame {
 	
 	public int p1score;
 	public int p2score;
 	
-	public String getScore(){
-		
+	public void getScore(){
+	
 	}
-	public String setScore(int p1score, p2score){
+	public void setScore(int p1score, int p2score){
 		this.p1score = p1score;
 		this.p2score = p2score;
 	}
