d32a054888a2bafb4f562af464adce2614ee515c
jannelau <jannelau@STUPID-PC709.stnet.local>
2016-09-01 15:50:09 +0300

    added variables

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,10 +1,14 @@
 
 public class TennisGame {
 	
+	public int p1score;
+	public int p2score;
+	
 	public String getScore(){
 		
 	}
-	public String setScore(){
-		
+	public String setScore(int p1score, p2score){
+		this.p1score = p1score;
+		this.p2score = p2score;
 	}
 }
