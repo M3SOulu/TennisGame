dbeb4b9a2326ef46c505576dc57770add39df45b
sjayathi <sjayathi@10.30.7.11>
2016-09-01 16:34:57 +0300

    Completion of tennis game with one test

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,11 +1,39 @@
 
 public class MainTest {
 	
-	public static void main(String args[]){
+	public static void main(String args[]){		
 		
-		TennisGame theGame = new TennisGame();
-		theGame.initScoreMap();
-		System.out.println(theGame.getScore(2, 2));
+		TennisGame theGame = new TennisGame();		//0-0
+		System.out.println(theGame.getScore());
+		theGame.player1Scored();				//15-0
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				//15-15
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				//15-30
+		System.out.println(theGame.getScore()); 
+		theGame.player1Scored();				//30-30
+		System.out.println(theGame.getScore()); 
+		theGame.player1Scored();				//40-30
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				//deuce
+		System.out.println(theGame.getScore()); 
+		theGame.player1Scored();				//Adv - 40
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				//deuce
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				//40 - adv
+		System.out.println(theGame.getScore()); 
+		theGame.player1Scored();				//deuce
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				//40 - adv
+		System.out.println(theGame.getScore()); 
+		theGame.player1Scored();				//deuce
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				//40 - adv
+		System.out.println(theGame.getScore()); 
+		theGame.player2Scored();				// 2 win
+		System.out.println(theGame.getScore());
+
 		
 	}
 
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -7,33 +7,61 @@ public class TennisGame {
 	private Integer player1Score;
 	private Integer player2Score;
 	
-	
-	public void initScoreMap(){
+	public TennisGame(){
+		scores.put(0, 0);
 		scores.put(1, 15);
 		scores.put(2, 30);
 		scores.put(3, 40);
+		
+		player1Score = 0;
+		player2Score = 0;
 	}
 	
-	public String getScore(Integer player1Score, Integer player2Score){
-		if(player1Score>3 || player2Score >3){
-			return "invalid score.";
+	public String getScore(){
+		Integer difference = scoreDifference(player1Score, player2Score);
+		
+				
+		if(player1Score >= 3 || player2Score >= 3){
+			if(player1Score == player2Score){
+				return "DEUCE";			
+			}
+			if(player1Score > 3 || player2Score > 3){
+			if(difference == 1){
+				return "ADVANTAGE - " + getTranslatedScore(player2Score);
+			}else if (difference == -1){
+				return getTranslatedScore(player1Score) + "- ADVANTAGE";
+			}
+			
+			if(difference == 2){
+				return "Player 1 Wins!";
+			}else if (difference == -2){
+				return "Player 2 Wins!";
+			}
+			}
 		}
-		
-		this.player1Score=player1Score;
-		this.player2Score=player2Score;
-		
-		//if(player1ScorescoreDifference(player1Score, player2Score)>1)
-		
-		return scores.get(player1Score) + "-" + scores.get(player2Score);
+		 		
+		return getTranslatedScore(player1Score) + "-" + getTranslatedScore(player2Score);
 	}
+	
+	public void player1Scored(){
+		player1Score++;
+	}
+	
+	public void player2Scored(){
+		player2Score++;
+	}
+	
 	
 	private Integer scoreDifference(Integer score1, Integer score2){
 		Integer difference = score1 - score2;
-		if(difference < 0){
-			difference = difference * -1;
-		}
-		
 		return difference;
+	}
+	
+	private Integer getTranslatedScore(Integer score){
+		if(score >3){
+			score = 3;
+		}
+		return scores.get(score);
 	}
 
 }
