fc09dfbb20bbd36d1c456fbd80758db067302866
jannelau <jannelau@STUPID-PC709.stnet.local>
2016-09-01 16:52:08 +0300

    I'm just piling everything I can think of into one method because this is great solution

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,10 +1,27 @@
-
-
-
 public class TennisGame {
 	
-	public int p1score;
-	public int p2score;
+	public int p1score = 0;
+	public int p2score = 0;
+	public String p1String = "love";
+	public String p2String = "love";
+	
+	public void Everything(){
+		if (p1score == 1){
+			p1String = "fifteen";
+		}else if(p1score == 2){
+			p1String = "thirty";
+		}else if(p1score == 3){
+			p1String = "forty";
+		}
+		
+		if (p2score == 1){
+			p2String = "fifteen";
+		}else if(p2score == 2){
+			p2String = "thirty";
+		}else if(p2score == 3){
+			p2String = "forty";
+		}
+	}
 	
 	public void getScore(){
 	
