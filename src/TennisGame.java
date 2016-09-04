629864938bda375c1ca18cc06f1fa73abefe325e
Aleksi Backman <bddubbb@gmail.com>
2016-09-04 13:09:45 +0000

    Remove useless tabs

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -11,45 +11,45 @@ public class TennisGame {
 	private int[] scores;
 	private boolean noAdvantage;
 	private int[] advantage;
-	
+
 	public static void main(String[] args) {
 		TennisGame aTennisGame = new TennisGame();
 		String winner = "0";
-		
+
 		aTennisGame.setup();
-		
+
 		while (winner.equals("0")) {
 			winner = aTennisGame.playRound(); 
 		}
-		
+
 		System.out.println("Winner is " + winner);
 	}
-	
+
 	private void setup() {
 		scores = new int[PLAYER_AMOUNT];
 		advantage = new int[PLAYER_AMOUNT];
 		noAdvantage = true;
-		
+
 		for (int i = 0; i < PLAYER_AMOUNT; i++) {
 			scores[i] = 0;
 			advantage[i] = 0;
 		}
 	}
-	
+
 	private String playRound() {
 		boolean playerOk = false;
 		String scoredPlayer = "";
-		
-		
+
+
 		while (!playerOk) {
 			System.out.println("Who scores? > ");
 			scoredPlayer = whoScored();
 			playerOk = validPlayer(scoredPlayer);
-			
+
 			if (!playerOk)
 				System.out.println("invalid player");
 		}
-		
+
 		if (didPlayerWin(scoredPlayer)) {
 			System.out.println(scoredPlayer + " scored!");
 			System.out.println("his score is now: " + scores[Integer.parseInt(scoredPlayer) - 1]);
@@ -60,14 +60,14 @@ public class TennisGame {
 			return "0";
 		}
 	}
-	
+
 	private boolean didPlayerWin(String scoredPlayer) {
 		if (scores[Integer.parseInt(scoredPlayer) - 1] <= 15) {
 			scores[Integer.parseInt(scoredPlayer) - 1] = scores[Integer.parseInt(scoredPlayer) - 1] + 15;
 		} else if (scores[Integer.parseInt(scoredPlayer) - 1] == 30) {
 			scores[Integer.parseInt(scoredPlayer) - 1] = scores[Integer.parseInt(scoredPlayer) - 1] + 10;
 		}
-		
+
 		if (false == noAdvantage) {
 			advantage[PLAYER_1] = 0;
 			advantage[PLAYER_2] = 0;
@@ -76,51 +76,51 @@ public class TennisGame {
 
 		if ((scores[PLAYER_1] == 40) && (scores[PLAYER_2] == 40)) {
 			noAdvantage = false;
-			
+
 			advantage[Integer.parseInt(scoredPlayer) - 1] = 1;
 		}
-		
+
 		//handle advantage
 		if ((40 == scores[Integer.parseInt(scoredPlayer) - 1] && noAdvantage) ||
 				(40 == scores[Integer.parseInt(scoredPlayer) - 1] && advantage[Integer.parseInt(scoredPlayer) - 1] == 1)) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private boolean validPlayer(String player) {
 		String[] validPlayers = new String[PLAYER_AMOUNT];
 		validPlayers[0] = "1";
 		validPlayers[1] = "2";
-		
+
 		for (int i = 0; i < validPlayers.length; i++) {
 			if (player.equals(validPlayers[i])) {
 				return true; 
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private String whoScored () {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = "";
-		
+
 		try {
 			input = br.readLine();
 		} catch (IOException e) {
 			System.out.println("exception: " + e.getMessage());
 		}
-		
+
 		return input;
 	}
-	
+
 	public void player1Scored() {
 
 	}
-	
+
 	public void playerScored(int player_num) {
-		
+
 	}
 }
