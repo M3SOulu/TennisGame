368fef8f7ec91927997bfd4608d6450633f88aa4
Ari Ruokamo <ari.ruokamo@gmail.com>
2016-09-08 18:12:19 +0300

    Unit tests for Tennisgame. Based on findings fixed the bugs.

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -13,7 +13,53 @@ public class TennisGameTest {
 		// Assert
 		assertEquals("Initial score incorrect", "love - love", score);		
 	}
-	
+
+	@Test
+	public void testTennisGame_FirstPlayerWinsGameStraight()  throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		String score = game.getScore();
+		// Assert
+		assertEquals("Player 1 should have won","player1 wins",score);
+	}
+
+	@Test
+	public void testTennisGame_SecondPlayerWinsGameStraight()  throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		String score = game.getScore();
+		// Assert
+		assertEquals("Player 2 should have won","player1 wins",score);
+	}
+
+	@Test
+	public void testTennisGame_SecondPlayerHasAdvantage()  throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		String score = game.getScore();
+		// Assert
+		assertEquals("Player 2 should have advantage","player2 has advantage",score);
+	}
+
 	@Test
 	public void testTennisGame_EahcPlayerWin4Points_Score_Deuce() throws TennisGameException {
 		//Arrange
@@ -49,5 +95,29 @@ public class TennisGameTest {
 		
 	}	
 	
-	
+	@Test
+	public void testTennisGame_EahcPlayerWin5Points_Score_Deuce() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player2Scored();
+
+		game.player1Scored();
+		game.player2Scored();
+
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Tie score incorrect", "deuce", score);		
+	}
+
 }
