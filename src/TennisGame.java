207926194b0f2cb6f1326c1a0eb66989eb9b3295
patsijee <patsijee@STUPID-PC717.stnet.local>
2016-09-09 16:04:53 +0300

    Unfinnished

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -3,7 +3,15 @@ public class TennisGame
 {
 	private static int player1Points = 0;
 	private static int player2Points = 0;
-	private static String[] resultString = {"love", "15", "30", "40", "deuce", "advantage"} 
+	private static int minShowPoints = 0;
+	private static int maxShowPoints = 3;
+	
+	private static String[] resultString = {"love", "fifteen", "thirty", "forty"} 
+	private static String outOfRangeString = "out of range";
+	private static String deuce = "deuce";
+	private static String advantage = "advantage";
+	private static String player1 = "player1";
+	private static String player2 = "player2";
 	
 	public static void main(String[] args)
 	{
@@ -34,7 +42,14 @@ public class TennisGame
 	private static void showScore() 
 	{
 		System.out.println("points are: " + player1Points + " - " + player2Points);		
-		
+		if (player1Points >= minShowPoints && player2Points >= minShowPoints  )
+		{
+			if (player1Points <= maxShowPoints && player2Points <= maxShowPoints)
+		}
+		else
+		{
+			System.out.println(player1 + " " + resultString[player1Points] + " - " + player2 +);
+		}
 	}
 
 	public static void setScore(int set1, int set2)
