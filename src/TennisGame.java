931d9eaf0d7bfa8b8a7f35fab5798d29574d6b0e
GabrieleLoaptriello <Gabriele@LAPTOP-5DP341KS>
2016-11-11 16:36:02 +0100

    completata prima parte

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -15,29 +15,36 @@ public class TennisGame {
 		
 		if(player1.getScore() == 0 && player2.getScore() == 0) {
 			score = "Initial love - love";
-		} 
+		} else if(player1.getScore() == 40 && player2.getScore() == 40) {
+			score = ""
+		}
 	}
 	
 	String addScore(int p) {
+		String whoScores = "";
 		if(p == 1) {
 			if(player1.getScore() <= 30){
 				player1.setScore(15);
-				player1.setPointWin();	
+				player1.setPointWin();
+				whoScores = "player1 scores > ";
 			} else {
 				player1.setScore(10);
 				player1.setPointWin();
+				whoScores = "player1 scores >";
 			}
 		} else if(p == 2) {
 			if(player2.getScore() <= 30){
 				player2.setScore(15);
 				player2.setPointWin();
+				whoScores = "player2 scores";
 			} else {
 				player2.setScore(10);
 				player2.setPointWin();
+				whoScores = "player2 scores";
 			}
 		}
 		
-		return player1.getScore() + " - " + player2.getScore();
+		return whoScores;
 	}
 	
 }
