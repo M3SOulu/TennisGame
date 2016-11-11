4540e9be747eba365b7b0a94e0647cd527b5486e
simone.denisi <denisisimone@gmail.com>
2016-11-11 17:16:04 +0100

    #3

--- src/TestTennisGame.java
+++ src/TestTennisGame.java
@@ -1,11 +1,8 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import junit.framework.Assert;
-
-
 
 public class TestTennisGame {
  
