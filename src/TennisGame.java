aeaf461936bc0ad657558bff9b69dfe5498ff250
mauri <mauri@DESKTOP-UQN5DSD>
2016-11-11 21:18:54 +0100

    Tennis Game class implemented but its test cases still miss.

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,22 +1,56 @@
-
+import exceptions.CantGetScoreDescriptionException;
+import exceptions.GameAlreadyFinishedException;
 
 public class TennisGame {
-	
+
 	private Player player1;
 	private Player player2;
 	private String gameScore;
+	private final String FORMAT = " scores > Score: ";
 	
-	public TennisGame(Player player1, Player player2){
-		this.player1 = player1;
-		this.player2 = player2;
+	public TennisGame(String player1Name, String player2Name) {
+		this.player1 = new Player(player1Name);
+		this.player2 = new Player(player2Name);
 	}
 
-	public String getGameScore() {
+	public void setGameScore() throws CantGetScoreDescriptionException {
+
+		//Victory1
+		if (player1.getScore() >= 3 && player1.getScore() - player2.getScore() >= 2)
+			gameScore = player1.getName() + " wins";
+		//Victory2
+		else if (player2.getScore() >= 3 && player2.getScore() - player1.getScore() >= 2)
+			gameScore = player2.getName() + " wins";
+		//Both scores >=3
+		else if (player1.getScore() >= 3 && player2.getScore() >= 3) {
+			//Odd
+			if (player1.getScore() == player2.getScore())
+				gameScore = "deuce";
+			//Advantage1
+			else if (player1.getScore() - player2.getScore() == 1)
+				gameScore = "advantage - 40";
+			//Advantage2
+			else if (player1.getScore() - player2.getScore() == -1)
+				gameScore = "40 - advantage";
+		}
+		//Both scores <3
+		else
+			gameScore = player1.getScoreDescription() + " - " + player2.getScoreDescription();
+	}
+	
+	public String winningBallBy(String playerName) throws CantGetScoreDescriptionException, GameAlreadyFinishedException{
 		
-
-		return gameScore;
+		if(gameScore.contains("wins")) throw new GameAlreadyFinishedException();
+		
+		if( playerName.equals(player1.getName()) ){
+			player1.addPoint();
+			setGameScore();
+			return player1.getName() + FORMAT + gameScore;
+		}else{
+			player2.addPoint();
+			setGameScore();
+			return player2.getName() + FORMAT + gameScore;
+		}
 	}
-	
-	
-	
+
 }
