def88d10c63fa518f5aa7582ef94562390d5b45d
luigiba <baldarigigi@gmail.com>
2016-11-11 17:21:13 +0100

    fase di test per la classe match

--- src/Match.java
+++ src/Match.java
@@ -1,27 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * 
+ * Tennis Match
+ *
+ */
 public class Match {
-	private List<Point> points;
 	private int winnerPointPlayer1 = 0;
 	private int winnerPointPlayer2 = 0;
 	
-	public Match()
-	{
-		points = new ArrayList<>();
-	}
-	
-	public void addPoint( Point p )
-	{
-		points.add( p );
-	}
-	
-	private void winnerPoint(Point p){
-		if(p.getWinner().getId() == 1){
+	/**
+	 *
+	 * @param p point
+	 */
+	public void addPoint( Point p ){
+		if( p.getWinner().getId() == 1 ){
 			this.winnerPointPlayer1++;
-		}else{
+		}
+		else{
 			this.winnerPointPlayer2++;
 		}
-		
 	}
+	
+	public boolean isWon()
+	{
+		if( ! (winnerPointPlayer1 == 4 || winnerPointPlayer2 == 4) ){
+			return false;
+		}
+		if( ! ( winnerPointPlayer1 >= (winnerPointPlayer2 + 2) ||
+				 winnerPointPlayer2 >= (winnerPointPlayer1 + 2))) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	
 }
---
+++ tests/MatchTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MatchTest {
+	Match m;
+	
+	@Before
+	public void setUp()
+	{
+		m = new Match();
+	}
+	
+	@Test
+	public void testAddPointWithPlayer1Winner() throws InvalidPlayerException, InvalidScoreException {
+		m.addPoint( new Point(new Player(new Score(1)), new Player(new Score(0) ) ));
+		assertEquals(expected, actual);
+	}
+
+}
