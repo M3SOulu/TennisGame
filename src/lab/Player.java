19905338b54e46aa65a9eb6ddd3f5f958a16d787
VittorioPennella <v.pennella1@studenti.uniba.it>
2016-11-13 19:35:14 +0100

    Completati i primi punti

---
+++ src/lab/Player.java
@@ -0,0 +1,15 @@
+package lab;
+
+public class Player {
+	private String player;
+
+public Player(String player){
+	this.player = player;
+	
+}
+
+public String getPlayer(){
+	return this.player;
+}
+
+}
