b6e0503c8de637c6cb9d0c8e5bba1718741cf85f
GabrieleLoaptriello <Gabriele@LAPTOP-5DP341KS>
2016-11-14 12:05:41 +0100

    added test class

---
+++ src/TennisGameTest/TennisGameTest.java
@@ -0,0 +1,19 @@
+package TennisGameTest;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TennisGameTest {
+
+	@Before
+	public static void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
