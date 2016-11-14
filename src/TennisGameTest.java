bc7b487c3010f180291cec57f3cc0524f2f056c1
GabrieleLoaptriello <Gabriele@LAPTOP-5DP341KS>
2016-11-14 12:30:30 +0100

    added coverage tests for "convertScores"

--- src/TennisGameTest.java
+++ src/TennisGameTest.java
@@ -1,21 +1,46 @@
 
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest {
+
 	private TennisGame tennisGame;
 	
 	@Before
-	public static void setUp() throws Exception {
-	
+	public void setUp() throws Exception {
+		tennisGame = new TennisGame();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void scoreIsZero() throws InvalidScoreException {
+		assertEquals(tennisGame.convertScores(0), "love");
 	}
-
+	
+	@Test
+	public void scoreIsOne() throws InvalidScoreException {
+		assertEquals(tennisGame.convertScores(1), "15");
+	}
+	
+	@Test
+	public void scoreIsTwo() throws InvalidScoreException {
+		assertEquals(tennisGame.convertScores(2), "30");
+	}
+	
+	@Test
+	public void scoreIsThree() throws InvalidScoreException {
+		assertEquals(tennisGame.convertScores(3), "40");
+	}
+	
+	@Test
+	public void scoreIsMoreThanThree() throws InvalidScoreException {
+		assertEquals(tennisGame.convertScores(4), "advantage");
+	}
+	
+	@Test(expected = InvalidScoreException.class)
+	public void invalidScore() throws InvalidScoreException {
+		assertEquals(tennisGame.convertScores(-1), "buongiornissimo, caffeeeeeee");
+	}
 }
