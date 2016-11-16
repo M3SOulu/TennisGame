fd2ca6f99318811c7ee7ec0380842aa9dc4c49c0
sarfilant <f.sarcinelli1@studenti.uniba.it>
2016-11-16 10:48:34 +0100

    1

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -41,10 +41,12 @@ public class TennisGame {
 		} else if (playerOne.getPoint() == 40) {
 			if (playerTwo.getPoint() < 40) {
 				result = playerOne.getName() + " scores > Score: " + playerOne.getName() + " wins";
-			} else if (playerTwo.getPoint() == 40 && playerTwo.isAdvantage() == false && playerOne.isAdvantage() == false) {
+			} else if (playerTwo.getPoint() == 40 && playerTwo.isAdvantage() == false
+					&& playerOne.isAdvantage() == false) {
 				playerOne.setAdvantage(true);
 				result = playerOne.getName() + " scores > Score: advantage - " + playerTwo.getPoint();
-			} else if (playerTwo.getPoint() == 40 && playerTwo.isAdvantage() == false && playerOne.isAdvantage() == true){
+			} else if (playerTwo.getPoint() == 40 && playerTwo.isAdvantage() == false
+					&& playerOne.isAdvantage() == true) {
 				result = playerOne.getName() + " scores > Score: " + playerOne.getName() + " wins";
 			} else if (playerTwo.getPoint() == 40 && playerTwo.isAdvantage() == true) {
 				playerTwo.setAdvantage(false);
@@ -59,46 +61,51 @@ public class TennisGame {
 
 	public String scorePlayerTwo() {
 		String result = null;
-		
+
 		if (playerTwo.getPoint() == 0) {
 			playerTwo.setPoint(15);
-			if (playerOne.getPoint() == 0){
+			if (playerOne.getPoint() == 0) {
 				result = playerTwo.getName() + " scores > Score: love - " + playerTwo.getPoint();
 			} else {
-				result = playerTwo.getName() + " scores > Score: " + playerOne.getPoint() + " - " + playerTwo.getPoint();
+				result = playerTwo.getName() + " scores > Score: " + playerOne.getPoint() + " - "
+						+ playerTwo.getPoint();
 			}
 		} else if (playerTwo.getPoint() == 15) {
 			playerTwo.setPoint(15);
-			if (playerOne.getPoint() == 0){
+			if (playerOne.getPoint() == 0) {
 				result = playerTwo.getName() + " scores > Score: love - " + playerTwo.getPoint();
 			} else {
-				result = playerTwo.getName() + " scores > Score: " + playerOne.getPoint() + " - " + playerTwo.getPoint();
+				result = playerTwo.getName() + " scores > Score: " + playerOne.getPoint() + " - "
+						+ playerTwo.getPoint();
 			}
 		} else if (playerTwo.getPoint() == 30) {
 			playerTwo.setPoint(10);
-			if (playerOne.getPoint() == 0){
+			if (playerOne.getPoint() == 0) {
 				result = playerTwo.getName() + " scores > Score: love - " + playerTwo.getPoint();
 			} else if (playerOne.getPoint() == 40) {
 				result = playerTwo.getName() + " scores > Score: deuce";
 			} else {
-				result = playerTwo.getName() + " scores > Score: " + playerOne.getPoint() + " - " + playerTwo.getPoint();
+				result = playerTwo.getName() + " scores > Score: " + playerOne.getPoint() + " - "
+						+ playerTwo.getPoint();
 			}
-		} else if (playerTwo.getPoint() == 40){
+		} else if (playerTwo.getPoint() == 40) {
 			if (playerOne.getPoint() < 40) {
 				result = playerTwo.getName() + " scores > Score: " + playerTwo.getName() + " wins";
-			} else if (playerOne.getPoint() == 40 && playerOne.isAdvantage() == false && playerTwo.isAdvantage() == false) {
+			} else if (playerOne.getPoint() == 40 && playerOne.isAdvantage() == false
+					&& playerTwo.isAdvantage() == false) {
 				playerTwo.setAdvantage(true);
 				result = playerTwo.getName() + " scores > Score: " + playerOne.getPoint() + " - advantage";
-			} else if (playerOne.getPoint() == 40 && playerOne.isAdvantage() == false && playerTwo.isAdvantage() == true){
+			} else if (playerOne.getPoint() == 40 && playerOne.isAdvantage() == false
+					&& playerTwo.isAdvantage() == true) {
 				result = playerTwo.getName() + " scores > Score: " + playerTwo.getName() + " wins";
 			} else if (playerOne.getPoint() == 40 && playerOne.isAdvantage() == true) {
 				playerOne.setAdvantage(false);
 				result = playerTwo.getName() + " scores > Score: deuce";
-			} else if (playerTwo.isAdvantage() == true){
+			} else if (playerTwo.isAdvantage() == true) {
 				result = playerTwo.getName() + " scores > Score: " + playerTwo.getName() + " wins";
 			}
 		}
-		
+
 		return result;
 	}
 }
